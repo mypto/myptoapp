98a417e3cab262ff2857e1cc4528c7e073b8c1ce
Ry <changeagent101@gmail.com>
2024-11-03 16:54:41 -0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>MyPTO App</h1>
+      {/* Add your original app components and logic here */}
+    </div>
+  )
+}
+
+export default App
